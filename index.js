cbb4a92ecf5a71200fdb3ed1934166a197509896
Lekan Ikotun <lekan.ikotun@aexp.com>
2018-05-11 09:07:05 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const requiredFields = [
+	'sourcecode',
+	'iacode',
+	'pmcCode',
+	'shortName',
+	'productName',
+	'cardHeadline',
+	'cardTitle',
+	'filters',
+	'similarCards',
+	'active',
+	'decommissioned',
+	'rates',
+	'fee',
+	'benefits',
+	'welcomeOffer'
+];
